e98218b782373c319bc65dff9b7f97bf8dd2a8b5
sawel <sawel@yandex.ru>
2018-11-19 12:35:34 +0800

    2 й коммит

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpService} from './http.service';
 import {Emoji} from './emoji';
+import {Router, Event, NavigationStart, ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd} from '@angular/router';
+import {Title} from '@angular/platform-browser';
+import {Observable} from 'rxjs';
+import {filter, map, mergeMap} from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -8,17 +12,15 @@ import {Emoji} from './emoji';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    emojis: Emoji[] = [];
+    title = 'Angular';
 
-    constructor(private httpService: HttpService) {
+    constructor(private httpService: HttpService,
+                private router: Router,
+                private activatedRoute: ActivatedRoute,
+                private titleService: Title) {
     }
 
     ngOnInit(): void {
-        this.httpService.getData()
-            .subscribe((data: any) => {
-                console.log(this.emojis = data);
-                console.log(typeof this.emojis);
-                console.log(this.emojis.length);
-            });
+        this.httpService.getListAll();
     }
 }
