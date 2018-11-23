32b6b9fbf76c68c7551a89da8d0e0cf4797bdf46
sawel <sawel@yandex.ru>
2018-11-23 15:25:19 +0800

    Доработка v1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {EmojiLikeComponent} from './emoji-like/emoji-like.component';
 import {EmojiDelComponent} from './emoji-del/emoji-del.component';
 import {EmojiAllComponent} from './emoji-all/emoji-all.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
     {path: 'all', component: EmojiAllComponent, data: {title: 'Все'}/*, resolve: {allList: httpService}*/},
     {path: 'like', component: EmojiLikeComponent, data: {title: 'Любимые'}},
     {path: 'del', component: EmojiDelComponent, data: {title: 'Удаленные'}},
-    {path: '', redirectTo: '/all', pathMatch: 'full'}
+    {path: '', redirectTo: '/all', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
